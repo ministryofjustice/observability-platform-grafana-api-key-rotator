7c114a97127d67ed7de27dee4b5fb043122207be
ep-93 <109581241+ep-93@users.noreply.github.com>
2026-06-03 12:06:36 +0100

    Update function.py (#795)
    * Update function.py
    
    Signed-off-by: ep-93 <109581241+ep-93@users.noreply.github.com>
    
    * Update function.py
    
    Signed-off-by: ep-93 <109581241+ep-93@users.noreply.github.com>
    
    * Update function.py
    
    Signed-off-by: ep-93 <109581241+ep-93@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: ep-93 <109581241+ep-93@users.noreply.github.com>
    Co-authored-by: Aaron Robinson <41325732+ASTRobinson@users.noreply.github.com>

--- src/var/task/function.py
+++ src/var/task/function.py
@@ -5,7 +5,7 @@ import botocore.exceptions
 
 grafana_client = boto3.client("grafana")
 secretsmanager_client = boto3.client("secretsmanager")
-workspace_api_key_name = os.environ["WORKSPACE_API_KEY_NAME"]
+workspace_service_account_name = os.environ["WORKSPACE_SERVICE_ACCOUNT_NAME"]
 workspace_api_key_ttl = (
     os.environ.get("WORKSPACE_API_KEY_TTL") or 1209600
 )  # 1209600 is 14 days
@@ -14,26 +14,52 @@ secret_id = os.environ["SECRET_ID"]
 
 
 def lambda_handler(event, context):  # pylint: disable=unused-argument
-    try:
-        grafana_client.delete_workspace_api_key(
-            keyName=workspace_api_key_name, workspaceId=workspace_id
+    # Find the service account ID by name
+    accounts = grafana_client.list_workspace_service_accounts(workspaceId=workspace_id)
+    service_account = next(
+        (
+            a
+            for a in accounts["serviceAccounts"]
+            if a["name"] == workspace_service_account_name
+        ),
+        None,
+    )
+    if service_account is None:
+        raise ValueError(
+            f"Service account '{workspace_service_account_name}' not found in workspace {workspace_id}"
         )
 
-    except botocore.exceptions.ClientError as e:
-        if e.response["Error"]["Code"] == "ResourceNotFoundException":
-            pass
-        else:
-            raise e
+    service_account_id = service_account["id"]
 
-    create_workspace_api_key = grafana_client.create_workspace_api_key(
-        keyName=workspace_api_key_name,
-        keyRole="ADMIN",
+    # Delete any existing tokens to avoid hitting limits
+    existing_tokens = grafana_client.list_workspace_service_account_tokens(
+        workspaceId=workspace_id,
+        serviceAccountId=service_account_id,
+    )
+    for token in existing_tokens.get("serviceAccountTokens", []):
+        try:
+            grafana_client.delete_workspace_service_account_token(
+                tokenId=token["id"],
+                serviceAccountId=service_account_id,
+                workspaceId=workspace_id,
+            )
+        except botocore.exceptions.ClientError as e:
+            if e.response["Error"]["Code"] == "ResourceNotFoundException":
+                pass
+            else:
+                raise e
+
+    # Create a new token
+    new_token = grafana_client.create_workspace_service_account_token(
+        name="terraform",
         secondsToLive=workspace_api_key_ttl,
+        serviceAccountId=service_account_id,
         workspaceId=workspace_id,
     )
 
     secretsmanager_client.update_secret(
-        SecretId=secret_id, SecretString=create_workspace_api_key["key"]
+        SecretId=secret_id,
+        SecretString=new_token["serviceAccountToken"]["key"],
     )
 
     return {
